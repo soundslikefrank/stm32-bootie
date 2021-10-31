2204d1a65f1eeced7030339967712da83cd02c26
chmanie <code@chmanie.com>
2021-10-31 23:44:23 +0100

    manually flash binary firmware file

--- blblinky/src/main.rs
+++ blblinky/src/main.rs
@@ -1,11 +1,10 @@
-//! Demonstrate the use of a blocking `Delay` using TIM5 general-purpose timer.
+//! Demonstrate the use of a blocking `Delay` using the SYST (sysclock) timer.
 
 #![deny(unsafe_code)]
 #![allow(clippy::empty_loop)]
 #![no_main]
 #![no_std]
 
-// Halt on panic
 use panic_semihosting as _; // panic handler
 
 use cortex_m_rt::entry;
@@ -13,25 +12,13 @@ use stm32f4xx_hal as hal;
 
 use crate::hal::{pac, prelude::*};
 
-/* pub const FLASH_USER: u32 = 0x0808_0000;
-fn boot(scb: &mut cortex_m::peripheral::SCB) {
-    unsafe {
-        // let sp: u32 = *(FLASH_USER as *const u32);
-        let rv: usize = *((FLASH_USER + 4) as *const usize);
-        scb.vtor.write(FLASH_USER);
-        // cortex_m::register::msp::write(sp);
-        let function = core::mem::transmute::<usize, extern "C" fn() -> !>(rv);
-        function();
-    }
-} */
-
 #[entry]
 fn main() -> ! {
-    if let (Some(dp), Some(_cp)) = (
+    if let (Some(dp), Some(cp)) = (
         pac::Peripherals::take(),
         cortex_m::peripheral::Peripherals::take(),
     ) {
-        // Set up the LED. On the Mini-F4 it's connected to pin PC13.
+        // Set up the LED. On the Nucleo-446RE it's connected to pin PA5.
         let gpioa = dp.GPIOA.split();
         let mut led = gpioa.pa5.into_push_pull_output();
 
@@ -39,15 +26,15 @@ fn main() -> ! {
         let rcc = dp.RCC.constrain();
         let clocks = rcc.cfgr.sysclk(48.mhz()).freeze();
 
-        // Create a delay abstraction based on general-pupose 32-bit timer TIM5
-        let mut delay = hal::delay::Delay::tim5(dp.TIM5, &clocks);
+        // Create a delay abstraction based on SysTick
+        let mut delay = hal::delay::Delay::new(cp.SYST, &clocks);
 
         loop {
             // On for 1s, off for 1s.
             led.set_high();
-            delay.delay_ms(1_000_u32);
+            delay.delay_ms(1000_u32);
             led.set_low();
-            delay.delay_us(1_000_000_u32);
+            delay.delay_ms(1000_u32);
         }
     }
 
--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -1,5 +1,3 @@
-//! Demonstrate the use of a blocking `Delay` using TIM5 general-purpose timer.
-
 #![allow(clippy::empty_loop)]
 #![no_main]
 #![no_std]
@@ -7,18 +5,19 @@
 // Halt on panic
 use panic_semihosting as _; // panic handler
 
-use stm32f4xx_hal as _hal;
+mod blinky_arr;
 
+use blinky_arr::BYTES;
 use cortex_m_rt::entry;
+use hal::{flash::FlashExt, pac, prelude::*};
+use stm32f4xx_hal as hal;
 
 pub const FLASH_USER: u32 = 0x0800_8000;
 
 fn boot(scb: &mut cortex_m::peripheral::SCB) {
     unsafe {
-        // let sp: u32 = *(FLASH_USER as *const u32);
         let rv: usize = *((FLASH_USER + 4) as *const usize);
         scb.vtor.write(FLASH_USER);
-        // cortex_m::register::msp::write(sp);
         let function = core::mem::transmute::<usize, extern "C" fn() -> !>(rv);
         function();
     }
@@ -27,7 +26,26 @@ fn boot(scb: &mut cortex_m::peripheral::SCB) {
 #[entry]
 fn main() -> ! {
     if let Some(mut cp) = cortex_m::peripheral::Peripherals::take() {
-        boot(&mut cp.SCB);
+        let mut dp = pac::Peripherals::take().unwrap();
+
+        let mut flash = dp.FLASH.unlocked();
+        // sector 2 is from 0x0800_8000 to 0x0800_BFFF
+        // erase more sectors if program is bigger than 16kb
+        if let Err(err) = flash.erase(2) {
+            let _err = err;
+        }
+
+        match flash.program(0x8000, BYTES.iter()) {
+            Ok(()) => {
+                drop(flash);
+                // boot user program
+                boot(&mut cp.SCB);
+            }
+            Err(err) => {
+                // FIXME: handle error
+                let _err = err;
+            }
+        }
     }
 
     loop {}
